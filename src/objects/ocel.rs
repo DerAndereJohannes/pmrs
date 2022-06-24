d28a537c59082c5f1d807c7c178a29bc95bc2866
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2022-06-24 23:44:27 +0200

    deserialize Ocel back to standard keys and fixed validator

--- src/objects/ocel.rs
+++ src/objects/ocel.rs
@@ -9,35 +9,35 @@ use std::collections::{HashMap, HashSet};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Ocel {
-    #[serde(alias = "ocel:global-log")]
+    #[serde(alias = "ocel:global-log", rename(serialize = "ocel:global-log"))]
     pub global_log: HashMap<String, Value>,
-    #[serde(alias = "ocel:global-event")]
+    #[serde(alias = "ocel:global-event", rename(serialize = "ocel:global-event"))]
     pub global_event: HashMap<String, Value>,
-    #[serde(alias = "ocel:global-object")]
+    #[serde(alias = "ocel:global-object", rename(serialize = "ocel:global-object"))]
     pub global_object: HashMap<String, Value>,
-    #[serde(alias = "ocel:objects")]
+    #[serde(alias = "ocel:objects", rename(serialize = "ocel:objects"))]
     pub objects: HashMap<String, OcelObject>,
-    #[serde(alias = "ocel:events")]
+    #[serde(alias = "ocel:events", rename(serialize = "ocel:events"))]
     pub events: IndexMap<String, OcelEvent>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct OcelObject{
-    #[serde(alias = "ocel:type")]
+    #[serde(alias = "ocel:type", rename(serialize = "ocel:type"))]
     pub obj_type: String,
-    #[serde(alias = "ocel:ovmap")]
+    #[serde(alias = "ocel:ovmap", rename(serialize = "ocel:ovmap"))]
     pub ovmap: HashMap<String, Value>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct OcelEvent {
-    #[serde(alias = "ocel:activity")]
+    #[serde(alias = "ocel:activity", rename(serialize = "ocel:activity"))]
     pub activity: String,
-    #[serde(alias = "ocel:timestamp")]
+    #[serde(alias = "ocel:timestamp", rename(serialize = "ocel:timestamp"))]
     pub timestamp: DateTime<Utc>,
-    #[serde(alias = "ocel:omap")]
+    #[serde(alias = "ocel:omap", rename(serialize = "ocel:omap"))]
     pub omap: HashSet<String>,
-    #[serde(alias = "ocel:vmap")]
+    #[serde(alias = "ocel:vmap", rename(serialize = "ocel:vmap"))]
     pub vmap: HashMap<String, Value>
 
 }
--- src/objects/ocel/validator/variants/jsonocel.rs
+++ src/objects/ocel/validator/variants/jsonocel.rs
@@ -1,23 +1,22 @@
 use jsonschema::JSONSchema;
-use serde_json::to_value;
-use std::error::Error;
-use crate::objects::ocel::importer::import_ocel;
+use serde_json::Value;
+use std::{error::Error, fs};
 
 pub(crate) fn validate_json(file_path: &str) -> Result<bool, Box<dyn Error>> {
-    let log = import_ocel(file_path)?;
+    let log = fs::read_to_string(file_path)?;
     let schema =  serde_json::from_str(include_str!("schema.json")).expect("JSON schema has been moved?");
     let compiled = JSONSchema::compile(&schema).expect("Schema is not valid.");
+    let json_log: Value = serde_json::from_str(&log.as_str()).unwrap();
 
-    Ok(compiled.is_valid(&to_value(&log).unwrap()))
+    Ok(compiled.is_valid(&json_log))
 }
 
 pub(crate) fn validate_json_verbose(file_path: &str) -> Result<bool, Box<dyn Error>> {
-    let log = import_ocel(file_path)?;
+    let log = fs::read_to_string(file_path)?;
     let schema =  serde_json::from_str(include_str!("schema.json"))?;
     let compiled = JSONSchema::compile(&schema).expect("A valid Schema");
     
-    let json_log = to_value(&log).unwrap();
-    println!("{}", &json_log.to_string());
+    let json_log: Value = serde_json::from_str(&log.as_str()).unwrap();
     let result = compiled.validate(&json_log);
 
     if let Err(errors) = result {
