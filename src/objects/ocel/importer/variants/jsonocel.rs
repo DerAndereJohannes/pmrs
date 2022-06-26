052a2ac7d6aa48dc4a1b0c19cd57dbd32a33d93a
Johannes Günter Herforth <johannes.herforth@rwth-aachen.de>
2022-06-26 22:44:32 +0200

    loaded ocel into memory before deserializing, 2.5x improvement

--- src/objects/ocel/importer/variants/jsonocel.rs
+++ src/objects/ocel/importer/variants/jsonocel.rs
@@ -1,8 +1,10 @@
 use crate::objects::ocel::Ocel;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::Read;
 use std::error::Error;
 
 pub(crate) fn import_json_ocel(file_path: &str) -> Result<Ocel, Box<dyn Error>> {
-    Ok(serde_json::from_reader(BufReader::new(File::open(file_path)?))?)
+    let mut s = String::new();
+    File::open(file_path).unwrap().read_to_string(&mut s).unwrap();
+    Ok(serde_json::from_str(&s).unwrap())
 }
