e65346eed6875cc8e918b74b45a21c77fcd3d246
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2022-08-03 22:13:10 +0200

    added tests and corrections to event_point features

--- src/algo/transformation/ocel/features/event_point.rs
+++ src/algo/transformation/ocel/features/event_point.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use serde_json::Value;
 use strum::{EnumString, IntoStaticStr, Display, IntoEnumIterator};
 
-use crate::objects::{ocel::Ocel, ocdg::{Ocdg, Relations}};
+use crate::objects::{ocel::Ocel, ocdg::{Ocdg, Relations}, linker::link_objects};
 
 #[derive(EnumString, IntoStaticStr, Display, Hash, Eq, PartialEq)]
 pub enum EventPoint {
@@ -24,19 +24,23 @@ pub struct EventPointConfig<'a> {
 
 pub fn relations_created_counts(log: &Ocel, ocdg: &Ocdg, eid: &usize) -> Vec<usize> {
     let mut relation_counts: Vec<usize> = vec![0;Relations::iter().count()];
+    let linker = link_objects(&log.object_map, &ocdg.object_map);
+
     if let Some(e) = log.events.get(eid) {
         e.omap.iter()
-              .filter(|oid1| ocdg.irels.contains_key(oid1))
+              .filter(|oid1| ocdg.irels.contains_key(linker.get_by_left(oid1).expect("cannot fail")))
               .for_each(|oid1| {
-                  let relsrc = ocdg.irels.get(oid1).unwrap();
-                  relsrc.iter().filter(|(oid2, _)| e.omap.contains(oid2))
-                               .for_each(|(_oid2, rels)| {
-                                   for (rel, rel_events) in rels {
-                                       if rel_events.contains(eid) {
-                                           relation_counts[*rel as usize] += 1; 
+                  let oid1_convert = linker.get_by_left(oid1).expect("cannot fail");
+                  if let Some(relsrc) = ocdg.irels.get(oid1_convert) {
+                      relsrc.iter().filter(|(oid2, _)| e.omap.contains(linker.get_by_right(oid2).expect("cannot fail")))
+                                   .for_each(|(_oid2, rels)| {
+                                       for (rel, rel_events) in rels {
+                                           if rel_events.contains(eid) {
+                                               relation_counts[*rel as usize] += 1; 
+                                           }
                                        }
-                                   }
-                               })
+                                   })
+                  }
               })
     }
     relation_counts
@@ -98,4 +102,73 @@ pub fn activity_ohe(log: &Ocel, eid: &usize) -> Vec<u8> {
         }
     }
     activity_bools
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::objects::{ocel::importer::import_ocel, ocdg::{generate_ocdg, Relations}};
+
+    use super::*;
+
+    lazy_static::lazy_static!{
+        static ref OCEL: Ocel = import_ocel("logs/ocel-complex-test.jsonocel").expect("What did you do to the file?");
+        static ref OCDG: Ocdg = generate_ocdg(&import_ocel("logs/ocel-complex-test.jsonocel").expect("What did you do to the file?"), &vec![Relations::DESCENDANTS]);
+    }
+
+    #[test]
+    fn test_relations_creates_counts() {
+        assert_eq!(relations_created_counts(&OCEL, &OCDG, &14)[4], 1);
+        assert_eq!(relations_created_counts(&OCEL, &OCDG, &32)[4], 2);
+        assert_eq!(relations_created_counts(&OCEL, &OCDG, &12)[4], 3);
+        assert_eq!(relations_created_counts(&OCEL, &OCDG, &0)[4], 0);
+    }
+
+    #[test]
+    fn test_omap_type_counts() {
+        assert_eq!(omap_type_counts(&OCEL, &0)["order"], 1);
+        assert_eq!(omap_type_counts(&OCEL, &0)["item"], 2);
+
+        assert_eq!(omap_type_counts(&OCEL, &33)["route"], 1);
+        assert_eq!(omap_type_counts(&OCEL, &33)["item"], 3);
+        assert_eq!(omap_type_counts(&OCEL, &33)["package"], 1);
+    }
+
+    #[test]
+    fn test_output_object_type_counts() {
+        assert_eq!(output_object_type_count(&OCEL, &29)["package"], 1);
+        assert_eq!(output_object_type_count(&OCEL, &32)["route"], 1);
+        assert_eq!(output_object_type_count(&OCEL, &1).is_empty(), true);
+    }
+
+    #[test]
+    fn test_input_object_type_counts() {
+        assert_eq!(input_object_type_count(&OCEL, &0).is_empty(), true);
+        assert_eq!(input_object_type_count(&OCEL, &33)["route"], 1);
+        assert_eq!(input_object_type_count(&OCEL, &33)["package"], 1);
+        assert_eq!(input_object_type_count(&OCEL, &33)["item"], 3);
+    }
+
+    #[test]
+    fn test_activity_ohe() {
+        let correct: HashMap<&str, u8> = HashMap::from_iter([("place order", 1)]);
+        
+        activity_ohe(&OCEL, &0).iter().enumerate().for_each(|(i, val)| {
+            match val {
+                0 => {assert!(!correct.contains_key(&OCEL.activities[i].as_str()))},
+                _ => {assert_eq!(*val, correct[OCEL.activities[i].as_str()])}
+            }
+        
+        });
+        
+        let correct: HashMap<&str, u8> = HashMap::from_iter([("failed delivery", 1)]);
+        activity_ohe(&OCEL, &18).iter().enumerate().for_each(|(i, val)| {
+            match val {
+                0 => {assert!(!correct.contains_key(&OCEL.activities[i].as_str()))},
+                _ => {assert_eq!(*val, correct[OCEL.activities[i].as_str()])}
+            }
+        
+        });
+    }
+
 }
